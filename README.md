53d6d0bf62cab7e9c38eaddb448c71ddb941c4f6
kevald1963 <kevin_alderson@yahoo.co.uk>
2019-12-08 11:54:33 +0000

    Updated README with installation notes. Also added all final CRUD functionality to Python file.

--- README.md
+++ README.md
@@ -4,4 +4,17 @@ Welcome kevald1963,
 
 We have preinstalled all of the tools you need to get started.
 
-Happy coding!
+>>>
+
+Well not exactly, because the 'dnspython' library wasn't installed. You have to do this manually.
+If you forget to do it, you can still connect to MongoDB but can't do any CRUD oerations. It was 
+installed once as per the instructions in the CI course video but, unfortunately, Gitpod seems to
+'lose' the installed library sometimes, so has to be re-installed. Not sure why this happens. 
+Doesn't appear to be related to stopping the Gitpod project. Went round in circles trying to find 
+out what had went wrong! 
+
+On AWS Cloud 9 and similar platforms use: 'sudo pip3 install dnspython'.
+
+On Gitpod, sudo is not allowed so just use: 'pip3 install dnspython'.
+
+>>>
--- mongo_project.py
+++ mongo_project.py
@@ -8,6 +8,7 @@ COLLECTION_NAME = "myFirstMDB"
 def mongo_connect(url):
     try:
         conn = pymongo.MongoClient(url)
+        #print("Connected successfully to MongoDB!")
         return conn
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB: %s") % e
@@ -24,18 +25,112 @@ def show_menu():
     option = input("Enter option: ")
     return option
 
+def get_record():
+    print("")
+    first = input("Enter first name: ")
+    last = input("Enter last name: ")
+
+    try:
+        doc = coll.find_one({'first': first.title(), 'last': last.title()})
+    except:
+        print("Error on searching for document.")
+
+    # Handle a 'not found' condition. This is not an error as such. The record just isn't there!
+    if not doc:
+        print("")
+        print("Sorry, no results found for " + first.title() + " " + last.title() + ".")
+
+    return doc
+
+def find_record():
+    doc = get_record()
+    if doc:
+        print("")
+        for key, value in doc.items():
+            if key != '_id':
+                print(key + ": " + value)
+
+def edit_record():
+    doc = get_record()
+    if doc:
+        # Define an empty dictionary in which to build the update record.
+        update_doc = {}
+        print("")
+        for key, value in doc.items():
+            if key != '_id':
+                # Populate the dictionary with each entered value.
+                # Set the new value to title case before storing in dictionary.
+                new_value = input(key.title() + " [ " + value.title() + " ] : ")
+                update_doc[key] = new_value.title() 
+                # Not every value may be changed, so revert to the original value is set to null.
+                if update_doc[key] == "":
+                    update_doc[key] = value
+        
+        try:
+            coll.update_one(doc, {'$set': update_doc})
+            print("")
+            print("Document updated.")
+        except:
+            print("Error on updating document.")
+
+def delete_record():
+    doc = get_record()
+    if doc:
+        print("")
+        for key, value in doc.items():
+            if key != '_id':
+                print(key + ": " + value)
+
+        print("")
+        confirmation = input("Is this the document you want to delete?\n(Y or N) ")
+        print("")
+
+        if confirmation.lower() == 'y':
+            try:
+                coll.delete_one(doc)
+                print("Document deleted.")
+            except:
+                print("Error on deleting document.")
+        else:
+            print("Document not deleted!")
+
+def add_record():
+    print("")
+    first = input("Enter first name: ")
+    last = input("Enter last name: ")
+    dob = input("Enter date of birth: ")
+    gender = input("Enter gender: ")
+    hair_colour = input("Enter hair colour: ")
+    occupation = input("Enter occupation: ")
+    nationality = input("Enter nationality: ")
+
+    new_doc = {'first': first.title(), 
+               'last': last.title(),
+               'dob': dob,
+               'gender': gender.upper(),
+               'hair_colour': hair_colour.title(),
+               'occupation': occupation.title(),
+               'nationality': nationality.title()}
+
+    try:
+        coll.insert_one(new_doc)
+        print("")
+        print("Document inserted.")
+    except:
+        print("Error on inserting document.")
+
 def main_loop():
     while True:
-        # Call the menu defined above
+        # Call the menu defined above.
         option = show_menu()
         if option == '1':
-            print("Called option 1") 
+            add_record()
         elif option == '2':
-            print("Called option 2") 
+            find_record()
         elif option == '3':
-            print("Called option 3") 
+            edit_record()
         elif option == '4':
-            print("Called option 4") 
+            delete_record()
         elif option == '5':
             conn.close
             break
@@ -46,7 +141,7 @@ def main_loop():
 # Connect to the database passing in the environment variable set up with the connection string.
 conn = mongo_connect(MONGODB_URI)
 
-# Set our collection name on this database.
+# Set collection name on this database.
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
 main_loop()
